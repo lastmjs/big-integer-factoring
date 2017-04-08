014a0ca9205a1e68fb19db8b9613f3225576b3c2
Jordan Last <jordan.michael.last@gmail.com>
2017-04-08 16:06:43 -0600

    peerId

--- worker.js
+++ worker.js
@@ -1269,7 +1269,7 @@ function search() {
 onmessage = function(e) {
     const message = e.data;
 
-    peerIDs[message.peerID] = peerID;
+    peerIDs[message.peerID] = message.peerID;
 
   if (message.type === 'WORK_INFO') {
       if (bigInt(message.startIndex).greaterOrEquals(bigInt(message.stopIndex))) {
